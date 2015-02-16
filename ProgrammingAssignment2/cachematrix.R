78f4dd2425536abcfc5621f59b28df3d5d7729fb
Venkat Batchu <venkatbatchu@outlook.com>
2015-02-15 16:36:56 -0800

    New code is added to cachematrix.R template

--- ProgrammingAssignment2/cachematrix.R
+++ ProgrammingAssignment2/cachematrix.R
@@ -6,36 +6,36 @@
 ## get & set functions to get and set the matrix
 ## getInv  to provide inverse of a matrix from cache if is already computed
 ## otherwise comptute and provide
-## setInv set the value of the inverse of a matrix.
+## setInv set the value of the inverse of a matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-        m <- NULL
-        set <- function(y) {
-         x <<- y
-         m <<- NULL
-        }
-        get <- function() x
-     setInv <- function(inv) m <<- inv
-      getInv <- function() m
- 										        list(set = set, get = get,
-										             setInv = setInv,
-										             getInv = getInv)
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
   }
+  get <- function() x
+  setInv <- function(inv) m <<- inv
+  getInv <- function() m
+  list(set = set, get = get,
+       setInv = setInv,
+       getInv = getInv)
+}
 
 ## Write a short comment describing this function
 
 ## Return a matrix that is the inverse of 'x'
 
 cacheSolve <- function(x, ...) {
- 
- #Retrun inverse of matrix 'x'
- m <- x$getInv()
- if (!is.null(m)) {
-     message("Getting cached data ....")
-     return(m)
+  
+  #Retrun inverse of matrix 'x'
+  m <- x$getInv()
+  if (!is.null(m)) {
+    message("Getting cached data ....")
+    return(m)
   }
   data <-x$get()
   m <-solve(data)
   x$setInv(m)
   m
-  }
+}
